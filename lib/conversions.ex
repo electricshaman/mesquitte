0d29f7e4d15e7b34a732bd03041c0602c68d5c92
Jeff Smith <jeff@sohlsmith.com>
2016-06-30 18:38:04 -0500

    Add to_bool and to_integer for values already of those types

--- lib/conversions.ex
+++ lib/conversions.ex
@@ -1,7 +1,11 @@
 defmodule Mesquitte.Conversions do
   def to_int(true), do: 1
   def to_int(false), do: 0
+  def to_int(value) when is_integer(value),
+    do: value
 
   def to_bool(1), do: true
   def to_bool(0), do: false
+  def to_bool(value) when is_boolean(value),
+    do: value
 end
